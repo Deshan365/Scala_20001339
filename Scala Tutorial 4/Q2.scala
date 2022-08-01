272b7883045af5e434cae811eea8292a2ed82b80
Deshan365 <84279336+Deshan365@users.noreply.github.com>
2022-08-01 23:11:43 +0530

    Update Q2.scala

--- Scala Tutorial 4/Q2.scala
+++ Scala Tutorial 4/Q2.scala
@@ -1,17 +1,17 @@
 import scala.io.StdIn._
 
-object Q2 {
+object Q2 extends App {
     def main(args: Array[String]): Unit = {
 
         print("Enter Number: ")
         var x = readInt();
 
         x match {
-        case x if x <= 0 => println("Number entered is Negative/Zero")
+        case x if x <= 0 => println("Number is Negative/Zero")
 
-        case x if x%2==0 => println("Number entered is Even")
+        case x if x%2==0 => println("Number is Even")
 
-        case _ => println("Number entered is Odd")
+        case _ => println("Number is Odd")
        }
     }
 }
